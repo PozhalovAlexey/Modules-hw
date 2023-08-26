0543f51542dc328853820a8fac38dead2459fe2c
Alexey Pozhalov <pozhalov7@mail.ru>
2023-08-26 20:04:08 +0300

    Little edit

--- src/js/App/app.js
+++ src/js/App/app.js
@@ -1,4 +1,4 @@
 import Game, {readGameSaving as loadGame, GameSavingData, writeGameSaving as saveGame} from '../Game/game';
-
+console.log(Game,loadGame,GameSavingData,saveGame)
 const game = new Game();
 game.start();
