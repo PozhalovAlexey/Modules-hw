6916e6e938c30323b2520ad7f103019588a308b4
Alexey Pozhalov <pozhalov7@mail.ru>
2023-08-26 20:03:34 +0300

    Little edit

--- src/js/App/app.js
+++ src/js/App/app.js
@@ -1,12 +1,4 @@
-import Game from '../Game/game';
-import {GameSavingData} from "../Game/game";
-import loadGame from '../Game/game'
-import saveGame from '../Game/game'
-// import {Game,GameSavingData}, loadGame,saveGame from'../Game/game.js' Не понимаю почему не работает!!!!
-
-console.log(GameSavingData,loadGame,saveGame)
-
-
+import Game, {readGameSaving as loadGame, GameSavingData, writeGameSaving as saveGame} from '../Game/game';
 
 const game = new Game();
 game.start();
